d0658a25e0847461f6fe22ad75c2935be850653c
suhrobfrontend <sukhrobfrontend@gmail.com>
2023-01-24 01:13:06 +0500

    update

--- js/main.js
+++ js/main.js
@@ -1,20 +1,23 @@
 const elTimerText = document.querySelector("[data-timer-display]")
 
-const birthdayDate = new Date("March 15, 2023 00:00:00").getTime()
+const birthdayDate = new Date("Jan 22, 2023 01:06:00").getTime()
+let year;
+let days;
+let hours;
+let minutes;
+let seconds;
 
 const timer = setInterval(() => {
   const now = new Date().getTime()
   const date = birthdayDate - now
+  year = Math.trunc((date % (1000 * 60 * 60 * 24 * 12 * 365)) / 1000 / 60 / 60 / 24 / 365)
+  days = Math.trunc((date % (1000 * 60 * 60 * 24 * 365)) / 1000 / 60 / 60 / 24)
+  hours = Math.trunc((date % (1000 * 60 * 60 * 24)) / 1000 / 60 / 60)
+  minutes = Math.trunc((date % (1000 * 60 * 60)) / 1000 / 60)
+  seconds = Math.trunc((date % (1000 * 60)) / 1000)
 
-  const year = Math.trunc((date % (1000 * 60 * 60 * 24 * 365)) / 1000 / 60 / 60 / 24 / 365)
-  const days = Math.trunc((date % (1000 * 60 * 60 * 24 * 365)) / 1000 / 60 / 60 / 24)
-  const hours = Math.trunc((date % (1000 * 60 * 60 * 24)) / 1000 / 60 / 60)
-  const minutes = Math.trunc((date % (1000 * 60 * 60)) / 1000 / 60)
-  const seconds = Math.trunc((date % (1000 * 60)) / 1000)
-
-  if (days === 0 && hours === 0 && minutes === 0 && seconds === 0) {
-    elTimerText.innerHTML == "Tug'ilgan kuningiz kirib keldi tabriklayman!"
-    return
+  if (seconds < 0) {
+    return elTimerText.innerHTML = `Tug'ilgan kuningiz ${year * (-1)} yil, ${days * (-1)} kun, ${hours * (-1)} soat, ${minutes * (-1)} daqiqa, ${seconds * (-1)} soniya kun avval o'tib ketgan, <br> dasturni qayta ishga tushiring`
   }
 
   elTimerText.innerHTML = `Tug'ilgan kuningizga ${year} yil, ${days} kun, ${hours} soat, ${minutes} daqiqa, ${seconds} soniya qoldi`
