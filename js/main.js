5b96791b0c211788805a5cb29f0dd0f0162718d2
sukhrobbeksoatov <suhrobbek_soatov@mail.ru>
2023-03-15 00:34:26 +0500

    add changes

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const elTimerText = document.querySelector("[data-timer-display]")
 
-const birthdayDate = new Date("March 15, 2023 00:00:00").getTime()
+const birthdayDate = new Date("March 15, 2024 00:00:00").getTime()
 let year;
 let days;
 let hours;
@@ -17,8 +17,8 @@ const timer = setInterval(() => {
   seconds = Math.trunc((date % (1000 * 60)) / 1000)
 
   if (seconds < 0) {
-    return elTimerText.innerHTML = `Tug'ilgan kuningiz ${year * (-1)} yil, ${days * (-1)} kun, ${hours * (-1)} soat, ${minutes * (-1)} daqiqa, ${seconds * (-1)} soniya kun avval o'tib ketgan, <br> dasturni qayta ishga tushiring`
+    return elTimerText.innerHTML = `Tug'ilgan kuningiz ${year * (-1)} yil, ${days * (-1)} kun, ${hours * (-1)} soat, ${minutes * (-1)} daqiqa, ${seconds * (-1)} soniya avval o'tib ketgan, <br> dasturni qayta ishga tushiring`
   }
 
-  elTimerText.innerHTML = `Tug'ilgan kuningizga ${year} yil, ${days} kun, ${hours} soat, ${minutes} daqiqa, ${seconds} soniya qoldi`
+  elTimerText.innerHTML = `Keyingi tug'ilgan kuningizga ${year} yil, ${days} kun, ${hours} soat, ${minutes} daqiqa, ${seconds} soniya qoldi`
 }, 1000)
